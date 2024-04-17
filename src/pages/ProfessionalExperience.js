a9d7e0a5a0da14ced705da5b8bbd1972e9a665af
basyair7 <basyair.fathul@gmail.com>
2024-04-18 01:04:18 +0700

    add animate effect in pages

--- src/pages/ProfessionalExperience.js
+++ src/pages/ProfessionalExperience.js
@@ -7,10 +7,10 @@ const ProfessionalExperience = () => {
     <>
       <div class="container-fluid p-0">
         <Header />
-        <section class="resume-section animate__animated animate__fadeIn" id="professionalexperience">
+        <section class="resume-section" id="professionalexperience">
           <div class="resume-section-content">
             <h2 class="mb-5">Professional Experience</h2>
-
+            
             <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
               <div class="flex-grow-1">
                 <h4 class="mb-0">Project Manager Asistant, Design of Temperature and Humidity Control System at 20 kV Distribution Substation Cubicle With Fuzzy Logic Method - MBKM USK Unggul</h4>
@@ -18,15 +18,13 @@ const ProfessionalExperience = () => {
                   <b>·</b> Collaborated with PT PLN (Persero) UP2D Aceh to enchance the efficiency and relability of 20 kV switchgear using fuzzy logic <br />
                   <b>·</b> Successfully developed a novel control system to keep the humidity of 20 kV switchgear in electricity distribution Substation below 80% which may save up to 45% of electric power savings once switchgear heaters are used.
                 </div>
-                <p>
-
-                </p>
+                <p></p>
               </div>
               <div class="flex-shrink-0">
                 <span class="text-primary">Feb 2023 - Sep 2023(6 months)</span>
               </div>
             </div>
-
+            
             <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
               <div class="flex-grow-1">
                 <h4 class="mb-0">Chairman, Automatic Watering Plant - Student Organization Capacity Building Program (PPK Ormawa) 2022, DIKTI.</h4>
@@ -35,23 +33,23 @@ const ProfessionalExperience = () => {
                   <b>·</b> It can water itself and be managed remotely via a webpage. <br />
                   <b>·</b> As a leader, I coordinated the team and designed the prototype concept. <br />
                 </div>
-                <p>
-                </p>
+                <p></p>
               </div>
 
               <div class="flex-shrink-0">
                 <span class="text-primary">Aug 2022 - Dec 2022 (4 months)</span>
               </div>
             </div>
-
+            
             <div class="d-flex flex-column flex-md-row justify-content-between">
               <div class="flex-grow-1">
-                <h3 class="mb-0">Recovery Switchgear Medium Voltage Intern</h3>
-                <div class="subheading mb-3">Recovery Switchgear</div>
+                <h4 class="mb-0">Recovery Switchgear Medium Voltage Intern</h4>
+                <div class="subheading mb-2">Recovery Switchgear</div>
                 <div class="lead mb-8">
                   <b>·</b> Working together and inspecting the conditions of 20 kV switchgear at distribution substations in Aceh, such as checking for corona and cleaning.<br />
-                  <b>·</b> One of the Engineer teams working in the substation's repair and restoration of 20 kV switchgear.<br />
+                  <b>·</b> One of the Engineer teams working in the substation's repair and restoration of 20 kV switchgear. <br />
                 </div>
+                <p><br /></p>
               </div>
               <div class="flex-shrink-0">
                 <span class="text-primary">Feb 2022 - Jul 2022 (6 months)</span>
@@ -60,19 +58,19 @@ const ProfessionalExperience = () => {
 
             <div class="d-flex flex-column flex-md-row justify-content-between">
               <div class="flex-grow-1">
-                <h3 class="mb-0">Laboratory Asistant, Electrical Power System Laboratory</h3>
+                <h4 class="mb-0">Laboratory Asistant, Electrical Power System Laboratory</h4>
                 <div class="lead mb-8">
                   <b>·</b> Helper teach students how to use high voltage lab equipment.<br />
                   <b>·</b> Helper teach students how to install electrical installations for 1-story and 2-story houses.<br />
                   <b>·</b> Working together in a team to develop renewable electrical energy from solar power systems.<br />
                 </div>
+                <p></p>
               </div>
-
               <div class="flex-shrink-0">
                 <span class="text-primary">Aug 2021 - Nov 2023 (2 years and 3 months)</span>
               </div>
             </div>
-
+            
           </div>
         </section>
         <hr class="m-0" />
