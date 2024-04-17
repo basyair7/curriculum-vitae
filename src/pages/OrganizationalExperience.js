809f85feee3c0e4f82e7f0a46c72b31bf6a06060
basyair7 <basyair.fathul@gmail.com>
2024-04-18 00:56:13 +0700

    modified pages

---
+++ src/pages/OrganizationalExperience.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Header from "../components/Header";
+
+const OrganizationalExperience = () => {
+    return (
+        <>
+            <div class="container-fluid p-0">
+                <Header />
+                <section class="resume-section" id="organizationalexperience">
+                    <div class="resume-section-content">
+                        <h2 class="mb-5">Organizational Experience</h2>
+
+                        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div class="flex-grow-1">
+                                <h4 class="mb-0">Robotics Electrical Engineering Community (KROENG USK)</h4>
+                                <div class="subheading mb-3">Leader</div>
+                                <div class="lead mb-8">
+                                    <b>·</b> Successful achievement of more that 30 active members who are divided into 4 divisions; Non-Technical, Programming, Electrical, Mechanical, and Design divisions.<br />
+                                    <b>·</b> Succeeded in coordinating the teams of the Indonesian Robot Contest (KRI) competition in the Indonesian SAR Robot Contest (KRSRI), <b>National</b>, 2022.<br />
+                                    <b>·</b> Succeeded in coordinating the PLN ICE competition temas, and got the <b>1st runner up</b> position with the project title "Automatic Watering Plant Based on IoT Systems", <b>National</b>, 2022.<br />
+                                </div>
+                                <p></p>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <span class="text-primary">February 2022 - April 2023</span>
+                            </div>
+                        </div>
+
+                        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div class="flex-grow-1">
+                                <h4 class="mb-0">Information and Commutication, HIMATEKTRO USK</h4>
+                                <div class="subheading mb-3">Member</div>
+                                <div class="lead mb-8">
+                                    <b>·</b> Collaborate to create a HIMATEKTRO USK information system based on an Android application and awebsite as a current and high-quality instructive tool. <br />
+                                </div>
+                                <p></p>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <span class="text-primary">May 2021 - Feb 2023</span>
+                            </div>
+                        </div>
+
+                        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div class="flex-grow-1">
+                                <h4 class="mb-0">Student Creativity Program (PKM) 2021, DIKTI</h4>
+                                <div class="subheading mb-3">Member</div>
+                                <div class="lead mb-8">
+                                    <b>·</b> Creating a prototypeof a renewable energy power plant with a multi-hybrid system that combines three renewable energy generators; photovoltaic; vertical axis wind turbines; and micro-hydro<br />
+                                    <b>·</b> Serves to produce a hreate electrical power output and is more effective and efficient both in terms of time and the area used.<br />
+                                </div>
+                                <p></p>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <span class="text-primary">June 2021 - September 2021</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+                <hr class="m-0" />
+            </div>
+        </>
+    );
+};
+
+export default OrganizationalExperience;
