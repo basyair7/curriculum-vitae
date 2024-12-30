3e0a5cc69f854c14b20e40db34ec99cf3d0fdb2a
basyair7 <basyair.fathul@gmail.com>
2024-12-30 20:06:52 +0700

    modified and rename file header

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <Link className="navbar-brand js-scroll-trigger" to="/">
+                <span className="d-block d-lg-none">Fathul Basyair</span>
+                <span className="d-none d-lg-block">
+                    <img 
+                        src="assets/img/profile.jpg" 
+                        alt="..." 
+                        className="img-fluid img-profile rounded-circle mx-auto mb-2" 
+                    />
+                </span>
+            </Link>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#about">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#organization">Organization</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#education">Education</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#interests">Interests</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#projects">Projects</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
