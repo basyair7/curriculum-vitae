f993a36e6082d8f3c99cb68c5fbf8254f6d4c06b
basyair7 <basyair.fathul@gmail.com>
2024-12-30 20:21:32 +0700

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-// import React, {useEffect, useState} from "react";
 import { useEffect } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import Projects from "./pages/Projects";
-import Education from "./pages/Education";
-import ProfessionalExperience from "./pages/ProfessionalExperience";
+
 import Home from "./pages/Home";
-import Interest from "./pages/Interest";
-import Skills from "./pages/Skills";
+import ProfessionalExperience from "./pages/ProfessionalExperience";
 import OrganizationalExperience from "./pages/OrganizationalExperience";
+import Education from "./pages/Education";
+import Skills from "./pages/Skills";
+import Interest from "./pages/Interest";
+import Projects from "./pages/Projects";
 
 const ScrollToTop = () => {
-  const { pathname, hash } = useLocation();
+    const { pathname, hash } = useLocation();
 
-  useEffect(() => {
-    if (hash) {
-      const element = document.querySelector(hash);
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  }, [pathname, hash]);
+    useEffect(() => {
+        if (hash) {
+            const element = document.querySelector(hash);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }, [pathname, hash]);
 
-  return null;
+    return null;
 };
 
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/experience" element={<ProfessionalExperience />} />
-          <Route path="/organization" element={<OrganizationalExperience />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/interests" element={<Interest />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (<>
+        <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/experience" element={<ProfessionalExperience />} />
+                <Route path="/organization" element={<OrganizationalExperience />} />
+                <Route path="/education" element={<Education />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/interests" element={<Interest />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
+    </>);
 };
 
 export default App;
